3884fe08b4c3263a879192682a2de3ce42961f87
DNT <ducconit@gmail.com>
2024-05-18 01:39:28 +0700

    chore: thêm cấu hình master-slave cho mysql

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $host = env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
@@ -57,6 +57,12 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            'read' => [
+                'host' => explode(',', env('DB_READ_HOST', $host)),
+            ],
+            'write' => [
+                'host' => explode(',', env('DB_WRITE_HOST', $host)),
+            ],
         ],
 
         'mariadb' => [
