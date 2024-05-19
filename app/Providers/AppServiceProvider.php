a3d3df372df6690420e7af3b1c1459768fa9ef76
DNT <ducconit@gmail.com>
2024-05-19 16:40:31 +0700

    chore: đặt strict mode model trên môi trường dev

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Contracts\Config\Repository;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
@@ -16,6 +17,10 @@ class AppServiceProvider extends ServiceProvider
         $configRepository = $this->app->make(Repository::class);
         if ($configRepository->get('app.force_https', false)) {
             URL::forceScheme('https');
+        }
+
+        if ($this->app->environment('local')) {
+            Model::shouldBeStrict();
         }
     }
 
